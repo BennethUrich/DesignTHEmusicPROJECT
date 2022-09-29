21c4bfb10ddbc7acf1c37772d864fd5f37f974bf
URICH <107805174+BennethUrich@users.noreply.github.com>
2022-09-29 10:49:03 -0300

    commitei na main

---
+++ script.js
@@ -0,0 +1,109 @@
+console.log("Welcome to new era");
+
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('songs/1.m4a');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+let songs = [
+    {songName: "On Sight", filePath: "songs/1.m4a", coverPath: "covers/1.jpg"},
+    {songName: "BLKKK SKKKN HEAD", filePath: "songs/2.m4a", coverPath: "covers/2.jpg"},
+    {songName: "I Am A God", filePath: "songs/3.m4a", coverPath: "covers/3.jpg"},
+    {songName: "New Slaves", filePath: "songs/4.m4a", coverPath: "covers/4.jpg"},
+    {songName: "Hold My Liquor", filePath: "songs/5.m4a", coverPath: "covers/5.jpg"},
+    {songName: "I'm In It", filePath: "songs/6.m4a", coverPath: "covers/6.jpg"},
+    {songName: "Blood On The Leaves", filePath: "songs/7.m4a", coverPath: "covers/7.jpg"},
+    {songName: "Guilt Trip", filePath: "songs/8.m4a", coverPath: "covers/8.jpg"},
+    {songName: "Send It Up", filePath: "songs/9.m4a", coverPath: "covers/9.jpg"},
+    {songName: "Bound 2", filePath: "songs/10.m4a", coverPath: "covers/10.jpg"},
+]
+
+songItems.forEach((element, i)=>{ 
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+})
+ 
+
+// Handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+// Listen to Events
+audioElement.addEventListener('timeupdate', ()=>{ 
+    // Update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{ 
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${songIndex+1}.m4a`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=9){
+        songIndex = 0
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.m4a`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.m4a`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
